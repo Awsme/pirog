b585407c912a4767b876225f23018701077cf554
Awsme <gitawsme@gmail.com>
2017-03-31 11:22:39 +0300

    test

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Site Title</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,400i,500,500i,600,600i,700,700i,800" rel="stylesheet">
+	<link href="http://allfont.ru/allfont.css?fonts=freeset-bold-cyrillic" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div class="wrapper">
+		<div class="wrapper-header">
+			<div class="container">
+				<div class="header-top clearfix">
+					<div class="col-3">
+						<div class="header-top-logo">
+							<a href="#">
+								<div class="header-top-logo-img"><img src="images/header-logo.png" alt="Logo"></div>
+								<div class="header-top-logo-title">
+									<div class="header-top-logo-title-main">DNK<span>analysis</span></div>
+									<div class="header-top-descr">Медицинский центр</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="header-top-contact">
+							<div class="header-top-contact-icon"><img src="images/header-icon-1.png" alt=""></div>
+							<div class="header-top-contact-phone">8 800 600-30-20</div>
+							<div class="header-top-descr">Бесплатный многокональный номер</div>
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="header-top-language">
+							<div class="header-top-language-icon">
+								<img src="images/header-icon-2.png" alt="">
+							</div>
+							<div class="header-top-language-picker"><a href="#">Выбрать язык</a></div>
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="header-top-user_panel">
+							<a href="#" class="header-top-user_panel-link">Войти</a>
+							<a href="#" class="header-top-user_panel-link cart">Корзина</a>
+						</div>
+					</div>
+				</div>
+				<div class="header-bottom clearfix">
+					<div class="col-8">
+						<ul class="header-bottom-nav">
+							<li><a href="#">Главная</a></li>
+							<li><a href="#">ДНК-Тест</a></li>
+							<li><a href="#" class="active">Вопросы-Ответы</a></li>
+							<li><a href="#">О компании</a></li>
+							<li><a href="#">Контактные данные</a></li>
+						</ul>
+					</div>
+					<div class="col-4">
+						<div class="header-bottom-btn">
+							<a href="#">Заказать сейчас</a>
+						</div>
+					</div>
+				</div>
+				<section class="top-content">
+					<div class="row clearfix">
+						<div class="col-4">
+							<img src="images/top-content-img-1.png" alt="">
+						</div>
+						<div class="col-8">
+							<div class="top-content-info">
+								<h3>Анализ ДНК на отцовство, материнство</h3>
+								<div class="top-content-info-descr">Мы предлагаем простой, доступный, 21 генетический маркер домой Тестирование отцовства на 6500 рублей, С результатами в течение 6 рабочих дней с момента получения образцов<br> И более 99,99% точности.</div>
+								<div class="top-content-info-buy_now">
+									<a href="#">Заказать сейчас</a>
+								</div>
+								<div class="top-content-info-old_price">6500 Р</div>
+							</div>
+						</div>
+					</div>
+					<div class="top-content-offers clearfix">
+						<div class="col-4">
+							<div class="top-content-offers-item">
+								<img src="images/top-content-img-2.png" alt="">
+								<div class="top-content-offers-item-title">
+									<span class="top-content-offers-item-title-head">Мы гарантируем</span>
+									<span class="top-content-offers-item-title-descr">99,99% точность</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="top-content-offers-item">
+								<img src="images/top-content-img-3.png" alt="">
+								<div class="top-content-offers-item-title">
+									<span class="top-content-offers-item-title-head">Срок выполнения</span>
+									<span class="top-content-offers-item-title-descr">Всего 6 дней</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="top-content-offers-item">
+								<img src="images/top-content-img-4.png" alt="">
+								<div class="top-content-offers-item-title">
+									<span class="top-content-offers-item-title-head">Нам доверяют</span>
+									<span class="top-content-offers-item-title-descr">более 1009 клиентов</span>
+								</div>
+							</div>
+						</div>
+					</div>				
+				</section>
+			</div>
+		</div>
+		
+		<section class="content">
+			<div class="container">
+				<div class="content-main_info">
+					<div class="content-main_info-title">
+						<img src="images/content-img-1.png" alt="">
+						<h3>Домашний тест на отцовство и/или материнство</h3>
+					</div>
+					<p>
+						Комплект для тестирования ДНК для дома обеспечит конфиденциальность и точность, которые вы ищете вКомфорт вашего дома. Если вам интересно, тест поможет вам успокоиться. Домашнее тестирование на отцовство Самое простое и доступное решение. Образцы собираются в С химической точки зрения ДНК — это длинная полимерная молекула, состоящая из повторяющихся блоков <br>— нуклеотидов. Каждый нуклеотид состоит из азотистого основания, сахара (дезоксирибозы) и фосфатной группы.
+					</p>
+				</div>
+				<div class="content-main_info-detailed clearfix">
+					<div class="col-4">
+						<div class="content-main_info-detailed-block">
+							<div class="content-main_info-detailed-block-head">
+								<img src="images/content-icon-1.png" alt="">
+								<h3>Быстро</h3>
+							</div>
+							<div class="content-main_info-detailed-block-descr">
+								Более длительные сроки обработки Означает более короткое ожидание вашего Результаты ДНК-теста. Чем меньше времени Вы ждете, меньше стресса И тем быстрее вы можете двигаться дальше.
+							</div>
+						</div>
+					</div>
+					<div class="col-4">
+						<div class="content-main_info-detailed-block">
+							<div class="content-main_info-detailed-block-head">
+								<img src="images/content-icon-2.png" alt="">
+								<h3>Точно</h3>
+							</div>
+							<div class="content-main_info-detailed-block-descr">
+								Точные результаты означают нахождение Закрытие с полным душевным спокойствием. Испытательные лаборатории Аккредитована ISO17025, которая Золотой стандарт в тестировании ДНК Аккредитации.
+							</div>
+						</div>
+					</div>
+					<div class="col-4">
+						<div class="content-main_info-detailed-block">
+							<div class="content-main_info-detailed-block-head">
+								<img src="images/content-icon-3.png" alt="">
+								<h3>Анонимно</h3>
+							</div>
+							<div class="content-main_info-detailed-block-descr">
+								HomeDNAВыполнить непоколебимо Обеспечивает самого строгого клиента Конфиденциальности и конфиденциальности. Мы Знать, что такое конфиденциальность О том, что это значит для вас.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="content-main_info-detailed-advantages clearfix">
+					<div class="col-4">
+						<img src="images/content-icon-4.png" alt="">
+						<div class="content-main_info-detailed-advantages-title">
+							<h3>В компалекте</h3>
+							<p>Для тестирования заказа</p>
+						</div>
+					</div>
+					<div class="col-4">
+						<img src="images/content-icon-5.png" alt="">
+						<div class="content-main_info-detailed-advantages-title">
+							<h3>Удобный способы</h3>
+							<p>Оплаты и доставки</p>
+						</div>
+					</div>
+					<div class="col-4">
+						<img src="images/content-icon-6.png" alt="">
+						<div class="content-main_info-detailed-advantages-title">
+							<h3>Гарантируем</h3>
+							<p>Правильный результат</p>
+						</div>
+					</div>
+				</div>
+				<div class="content-main_info-offers clearfix">
+					<div class="col-6">
+						<div class="content-main_info-offers-img">
+							<img src="images/content-img-2.png" alt="">
+						</div>
+						<div class="content-main_info-offers-title">
+							<h3>Дружелюбное обслуживание клиентов. Чтобы ответить на ВСЕ ваши важные вопросы</h3>
+						</div>
+					</div>
+					<div class="col-6"></div>
+				</div>
+				<div class="content-main_info-offers"></div>
+			</div>
+		</section>
+		<div class="footer"></div>
+	</div>
+</body>
+</html>
