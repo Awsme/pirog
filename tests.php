01bfe80357f9b4708fe6d4d9cd363841c7289561
awsme <gitawsme@gmail.com>
2017-04-09 01:14:50 +0300

    add tests-page

---
+++ tests.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Site Title</title>
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+
+	<link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,400i,500,500i,600,600i,700,700i,800" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="http://allfont.ru/allfont.css?fonts=freeset-bold-cyrillic" rel="stylesheet" type="text/css" />
+
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="js/slick.min.js"></script>
+	<script type="text/javascript" src="../js/accordion.js"></script>
+
+</head>
+<body>
+	<div class="wrapper">
+		<div class="wrapper-header">
+			<div class="container">
+				<div class="header-top clearfix">
+					<div class="col-3">
+						<div class="header-top-logo">
+							<a href="#">
+								<div class="header-top-logo-img"><img src="images/header-logo.png" alt="Logo"></div>
+								<div class="header-top-logo-title">
+									<div class="header-top-logo-title-main">DNK<span>analysis</span></div>
+									<div class="header-top-descr">Медицинский центр</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="header-top-contact">
+							<div class="header-top-contact-icon"><img src="images/header-icon-1.png" alt=""></div>
+							<div class="header-top-contact-phone">8 800 600-30-20</div>
+							<div class="header-top-descr">Бесплатный многокональный номер</div>
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="header-top-language">
+							<div class="header-top-language-icon">
+								<img src="images/header-icon-2.png" alt="">
+							</div>
+							<div class="header-top-language-picker"><a href="#">Выбрать язык</a></div>
+						</div>
+					</div>
+					<div class="col-3">
+						<div class="header-top-user_panel">
+							<a href="#" class="btn btn-medium btn-style-2">Войти</a>
+							<a href="#" class="header-top-user_panel-link cart btn btn-medium btn-style-2">Корзина</a>
+						</div>
+					</div>
+				</div>
+				<div class="header-bottom clearfix">
+					<div class="col-8">
+						<ul class="header-bottom-nav">
+							<li><a href="#">Главная</a></li>
+							<li><a href="#" class="active">ДНК-Тест</a></li>
+							<li><a href="#">Вопросы-Ответы</a></li>
+							<li><a href="#">О компании</a></li>
+							<li><a href="#">Контактные данные</a></li>
+						</ul>
+					</div>
+					<div class="col-4">
+						<div class="header-bottom-btn">
+							<a href="#" class="btn btn-large btn-style-1">Заказать сейчас</a>
+						</div>
+					</div>
+				</div>
+				<section class="top-content">
+					<div class="row clearfix">
+						<div class="col-2">
+							<div class="top-content-page-icon">
+								<img src="images/tests-icon-1.png" alt="">
+							</div>
+						</div>
+						<div class="col-10">
+							<div class="top-content-info">
+								<div class="top-content-breadcrumbs">
+									<ul class="clearfix">
+										<li><a href="#">Главная</a></li>
+										<li><a href="#" class="active">ДНК тесты продажа</a></li>
+									</ul>
+								</div>
+								<h3>Все ДНК тесты на дому</h3>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+		<section class="content">
+			<div class="container">
+				<div class="row clearfix">
+					<div class="col-8">
+						<div class="content-main_info content-main_info-special clearfix">
+							<div class="col-12">
+								<h3 class="content-main_info-heading">Мы предлагаем широкий спектор ДНК тестов</h3>
+								<p class="content-main_info-heading-descrs">Представители действительно единственные в своем роде, предлагая Помочь на всем пути от принятия решения о Отцовство тест через понимание вашего теста Результаты. Свяжитесь с нами.</p>
+								<ul class="tests-list">
+									<li>
+										<div class="row clearfix">
+											<div class="col-2">
+												<img src="images/content-img-3.png">
+											</div>
+											<div class="col-8">
+												<h3>DNS test интересные вопросы</h3>
+												<p>Генеалогический ДНК тест исследует определенные части генома тестируемого человека. Результаты дают информацию о генеалогических связях и происхождении человека. </p>
+											</div>
+											<div class="col-2">
+												<p class="test_price">3 999 <span class="test_currency">P</span></p>
+												<a href="#" class="btn btn-medium btn-style-1 btn-disabled">Заказать</a>
+											</div>
+										</div>
+									</li>
+									<li>
+										<div class="row clearfix">
+											<div class="col-2">
+												<img src="images/content-img-3.png">
+											</div>
+											<div class="col-8">
+												<h3>DNS test интересные вопросы</h3>
+												<p>Генеалогический ДНК тест исследует определенные части генома тестируемого человека. Результаты дают информацию о генеалогических связях и происхождении человека. </p>
+											</div>
+											<div class="col-2">
+												<p class="test_price">3 999 <span class="test_currency">P</span></p>
+												<a href="#" class="btn btn-medium btn-style-1">Заказать</a>
+											</div>
+										</div>
+									</li>
+								</ul>
+								<div class="show-more center">
+									<a href="#" class="btn btn-medium btn-style-3">Показать еще</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-4">
+						<div class="sidebar">
+							<h3>Нужна помощь?</h3>
+							<p class="phone-icon">Закажите обратный звонок, бесплатно проконсультируем</p>
+							<div class="sidebar-buttons">
+								<div class="sidebar-button">
+									<a href="#" class="btn btn-medium btn-style-2">Обратный звонок</a>
+								</div>
+								<div class="sidebar-button">
+									<a href="#" class="btn btn-medium btn-style-1">Заказать тест сейчас</a>
+								</div>
+							</div>
+							<div class="sidebar-partners">
+								<h3>Наши партнеры</h3>
+								<p>Только проверенные лаборотории</p>
+								<ul>
+									<li><a href="#"><img src="images/sidebar-partners-img-1.png" alt=""></a></li>
+									<li><a href="#"><img src="images/sidebar-partners-img-2.png" alt=""></a></li>
+									<li><a href="#"><img src="images/sidebar-partners-img-3.png" alt=""></a></li>
+								</ul>
+							</div>
+							<div class="sidebar-howto">
+								<h3>Как сделать тест?</h3>
+								<p>Полная инструкция по использованию теста и его применения</p>
+								<a href="#" class="btn btn-medium btn-style-2">Вопросы-ответы</a>
+							</div>
+						</div>
+					</div>
+				</div>	
+				<div class="content-main_info contacts">
+					<div class="row clearfix">
+						<div class="col-6">
+							<h2>Контактные данные</h2>
+							<div class="contacts_info">
+								<div class="col-6">
+									<div class="address">
+										<h3>Адрес</h3>
+										<p>Bolshaya Novodmitrovskaya ul., 36 Moscow, 127015</p>
+									</div>
+									<div class="phone">
+										<h3>Номер телефона</h3>
+										<p>8 800 600-20-30</p>
+									</div>
+								</div>
+								<div class="col-6">
+									<div class="email">
+										<h3>Пишите нам</h3>
+										<p>info@dnkanalysis.com</p>
+										<a href="#" class="btn btn-large btn-style-2">Заказать звонок</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-6 contact_form">
+							<img src="images/content-img-2-icon.png" alt="">
+							<form action="#">
+								<div class="clearfix">
+									<div class="col-4">
+										<div class="form-group">
+											<label for="#" class="cf-label">Ваше имя</label>
+											<input type="text" class="cf-input" value="Елизавета">
+										</div>
+									</div>
+									<div class="col-4">
+										<div class="form-group cf-select-arrow">
+											<label for="#" class="cf-label">Тип сообщения</label>
+											<select class="cf-input cf-select">
+												<option value="1">Консультация</option>
+												<option value="2">Консультация</option>
+												<option value="3">Консультация</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-4">
+										<div class="form-group">
+											<label for="#" class="cf-label">Ваш e-mail</label>
+											<input type="text" class="cf-input">
+										</div>
+									</div>
+								</div>
+								<div class="clearfix">
+									<div class="col-12">
+										<div class="form-group">
+											<label for="#" class="cf-label">Сообщение</label>
+											<textarea name="" id="" class="message cf-textarea"></textarea>
+										</div>
+									</div>
+								</div>
+								<a href="#" class="btn btn-large btn-style-1">Отправить</a>
+							</form>
+						</div>	
+					</div>									
+				</div>			
+			</div>
+		</section>
+		<div class="footer">
+			<div class="footer-wrapper">
+				<div class="container">
+					<div class="footer-top clearfix">
+						<div class="col-3">
+							<div class="footer-top-logo">
+								<a href="#">
+									<div class="footer-top-logo-img"><img src="images/footer-logo.png" alt="Logo"></div>
+								</a>
+							</div>
+						</div>
+						<div class="col-3">
+							<div class="footer-top-contact">
+								<div class="footer-top-contact-icon"><img src="images/header-icon-1.png" alt=""></div>
+								<div class="footer-top-contact-phone">8 800 600-30-20</div>
+								<div class="footer-top-descr">Бесплатный многокональный номер</div>
+							</div>
+						</div>
+						<div class="col-3">
+							<div class="footer-top-language">
+								<div class="footer-top-language-icon">
+									<img src="images/header-icon-2.png" alt="">
+								</div>
+								<div class="footer-top-language-picker"><a href="#">Выбрать язык</a></div>
+							</div>
+						</div>
+						<div class="col-3">
+							<div class="footer-top-user_panel">
+								<a href="#" class="footer-top-user_panel-link btn btn-medium btn-style-2">Войти</a>
+								<a href="#" class="footer-top-user_panel-link cart btn btn-medium btn-style-2">Корзина</a>
+							</div>
+						</div>
+					</div>
+					<div class="footer-bottom clearfix">
+						<div class="col-8">
+							<ul class="footer-bottom-nav">
+								<li><a href="#">Главная</a></li>
+								<li><a href="#">ДНК-Тест</a></li>
+								<li><a href="#" class="active">Вопросы-Ответы</a></li>
+								<li><a href="#">О компании</a></li>
+								<li><a href="#">Контактные данные</a></li>
+							</ul>
+						</div>
+						<div class="col-4">
+							<div class="footer-bottom-btn">
+								<a href="#" class="btn btn-medium btn-style-1">Заказать сейчас</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+		$('.content-main_info-carousel').slick({
+			centerMode: true,
+			centerPadding: '0',
+			slidesToShow: 3,
+			responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						arrows: false,
+						centerMode: true,
+						centerPadding: '40px',
+						slidesToShow: 3
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						arrows: false,
+						centerMode: true,
+						centerPadding: '40px',
+						slidesToShow: 1
+					}
+				}
+			]
+		});
+	</script>
+</body>
+</html>
